a4bdb15c346fa0fa2143296bec0de9a547ed835b
dantecarlo <dante.cnp@gmail.com>
2022-04-11 15:58:52 -0500

    Fix type import

--- src/services/rickAndMorty.api.tsx
+++ src/services/rickAndMorty.api.tsx
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
-import { CharactersApi } from '../types/characters.types'
+import { ICharacterAPI } from '../types/characters.types'
 
 export const rickAndMortyApi = axios.create({
   baseURL: 'https://rickandmortyapi.com/api',
 })
 
 export const getCharacters = ({ page = 1 }) => {
-  return axios.get<CharactersApi>(
+  return axios.get<ICharacterAPI>(
     `https://rickandmortyapi.com/api/character/?page=${page}`,
   )
 }
